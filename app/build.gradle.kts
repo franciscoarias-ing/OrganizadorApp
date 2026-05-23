d5543f1aa3b4df8798bde17df70b7544f4935be4
francisco_ing <104174799+franciscoarias-ing@users.noreply.github.com>
2026-05-22 20:23:13 -0500

    nuevos fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,10 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
 }
---
+++ app/src/main/java/com/example/organizadorapps/ui/FavoritesFragment.kt
@@ -0,0 +1,17 @@
+package com.example.organizadorapps.ui
+
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FavoritesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return TextView(requireContext()).apply {
+            text = "Favoritos\nPróximamente apps favoritas"
+            textSize = 22f
+            setTextColor(android.graphics.Color.WHITE)
+            setPadding(40, 80, 40, 40)
+        }
+    }
+}
